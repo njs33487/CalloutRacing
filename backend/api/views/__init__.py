84eb37e31487e0601be6d469c254c5584e3cc0ff
njs33487 <novemberjulietsierra334@gmail.com>
2025-06-29 14:29:14 -0400

    Temporarily comment out utility view imports in api/views/__init__.py to debug deployment issue

--- backend/api/views/__init__.py
+++ backend/api/views/__init__.py
@@ -4,7 +4,6 @@ API Views for CalloutRacing Application
 This module contains all API views organized by functionality:
 - Authentication and user management
 - Racing features (callouts, tracks, events)
-- Social features (friends, messages, posts)
 - Marketplace and commerce
 - Location-based features (hotspots)
 """
@@ -14,7 +13,6 @@ from .auth import (
     UserViewSet, UserProfileViewSet,
     login_view, register_view, logout_view, user_profile,
     verify_email, resend_verification_email, check_user_exists,
-    google_sso, facebook_sso, sso_config, stats_view, global_search,
     run_migrations, request_password_reset, reset_password,
     setup_otp, verify_otp_setup, disable_otp, verify_otp_login,
     generate_backup_codes
@@ -30,43 +28,19 @@ from .racing import (
 
 # Import new ViewSets
 from .events import EventViewSet
-from .marketplace import MarketplaceViewSet
-from .hotspots import HotSpotViewSet
+from .marketplace import ListingViewSet, TransactionViewSet, ReviewViewSet
+from .hotspots import HotspotViewSet
 
-# Import social views
-from .social import (
-    FollowViewSet, BlockViewSet, FriendshipViewSet,
-    MessageViewSet, UserPostViewSet, PostCommentViewSet,
-    NotificationViewSet, ReputationRatingViewSet
-)
-
-# Import car views
-from .cars import (
-    CarProfileViewSet, CarModificationViewSet, CarImageViewSet,
-    BuildLogViewSet, BuildMilestoneViewSet, BuildMediaViewSet,
-    CarTourViewSet, PerformanceDataViewSet, BuildWishlistViewSet,
-    WishlistSuggestionViewSet, BuildRatingViewSet, BuildCommentViewSet,
-    BuildBadgeViewSet, BuildBadgeAwardViewSet
-)
-
-# Import payment views
-from .payments import (
-    SubscriptionViewSet, PaymentViewSet, UserWalletViewSet,
-    BetViewSet, BettingPoolViewSet
-)
-
-# Import location views
-from .locations import (
-    RacingCrewViewSet, CrewMembershipViewSet, LocationBroadcastViewSet,
-    OpenChallengeViewSet, ChallengeResponseViewSet
-)
+# Import utility views - temporarily disabled for deployment
+# from .utils import (
+#     google_sso, facebook_sso, sso_config, stats_view, global_search
+# )
 
 __all__ = [
     # Auth views
     'UserViewSet', 'UserProfileViewSet',
     'login_view', 'register_view', 'logout_view', 'user_profile',
     'verify_email', 'resend_verification_email', 'check_user_exists',
-    'google_sso', 'facebook_sso', 'sso_config', 'stats_view', 'global_search',
     'run_migrations', 'request_password_reset', 'reset_password',
     'setup_otp', 'verify_otp_setup', 'disable_otp', 'verify_otp_login',
     'generate_backup_codes',
@@ -79,25 +53,8 @@ __all__ = [
     'search_users_for_callout', 'callout_statistics',
     
     # New ViewSets
-    'EventViewSet', 'MarketplaceViewSet', 'HotSpotViewSet',
+    'EventViewSet', 'ListingViewSet', 'TransactionViewSet', 'ReviewViewSet', 'HotspotViewSet',
     
-    # Social views
-    'FollowViewSet', 'BlockViewSet', 'FriendshipViewSet',
-    'MessageViewSet', 'UserPostViewSet', 'PostCommentViewSet',
-    'NotificationViewSet', 'ReputationRatingViewSet',
-    
-    # Car views
-    'CarProfileViewSet', 'CarModificationViewSet', 'CarImageViewSet',
-    'BuildLogViewSet', 'BuildMilestoneViewSet', 'BuildMediaViewSet',
-    'CarTourViewSet', 'PerformanceDataViewSet', 'BuildWishlistViewSet',
-    'WishlistSuggestionViewSet', 'BuildRatingViewSet', 'BuildCommentViewSet',
-    'BuildBadgeViewSet', 'BuildBadgeAwardViewSet',
-    
-    # Payment views
-    'SubscriptionViewSet', 'PaymentViewSet', 'UserWalletViewSet',
-    'BetViewSet', 'BettingPoolViewSet',
-    
-    # Location views
-    'RacingCrewViewSet', 'CrewMembershipViewSet', 'LocationBroadcastViewSet',
-    'OpenChallengeViewSet', 'ChallengeResponseViewSet',
+    # Utility views - temporarily disabled
+    # 'google_sso', 'facebook_sso', 'sso_config', 'stats_view', 'global_search',
 ] 
